fa1787d86139afce1caa77d59e5137a828266846
Gabriel Diniz <gabrielvmdiniz12@gmail.com>
2026-06-09 10:15:59 -0300

    Feat: get params implemented

--- lib/request.ts
+++ lib/request.ts
@@ -2,14 +2,40 @@ import { IncomingMessage } from "node:http";
 
 class Request {
     private request: IncomingMessage;
-    public pathProvided: string | undefined;
-    public methodProvided: string | undefined;
+    public path: string | undefined;
+    public method: string | undefined;
+    public providedPath: string | undefined;
 
-    constructor(request: IncomingMessage) {
+    constructor(request: IncomingMessage, providedPath: string | undefined) {
         this.request = request;
-        this.pathProvided = request.url;
-        this.methodProvided = request.method;
+        this.path = request.url;
+        this.method = request.method;
+        this.providedPath = providedPath;
     };
+
+    getParams() {
+        let paramsName: any;
+        let params;
+        
+        if (this.providedPath?.includes(":")) {
+            paramsName = this.providedPath.match(/(?<=:).+/);
+        } else {
+            return {
+                "error": "Params not found"
+            };
+        }
+        
+        params = this.path?.match(/\/([^\/]*)$/);
+
+        let result;
+
+        if (params !== null && params !== undefined) {
+            result = {
+                [paramsName]: params[1]
+            }
+        }
+        return result;
+    }
 }
 
 export default Request;
